1d56808185c122254b47218685402e308b69080d
plumstyle <anxxx154@morris.umn.edu>
2018-09-05 16:31:33 -0500

    partial work for script 2

--- cleaning/big_clean.sh
+++ cleaning/big_clean.sh
@@ -4,4 +4,16 @@ DIR=$1
 
 SCRATCH=$2
 
-tar -C $SCRATCH -xf $DIR
+HERE=$(pwd)
+
+tar -xvzf $DIR -C $SCRATCH
+
+cd $SCRATCH
+
+grep -rl "DELETE ME!" . | xargs rm
+
+tar -cvf cleaned_little_dir.tgz little_dir/
+
+mv cleaned_little_dir.tgz $HERE
+
+
